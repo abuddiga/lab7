5ef5e67734b4c33d3dbc132c74f36ef376e9725a
Akshay Buddiga <abuddiga@stanford.edu>
2014-02-19 15:16:19 -0800

    Viewing, adding, and deleting projects

--- routes/project.js
+++ routes/project.js
@@ -3,8 +3,7 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
-  // query for the specific project and
-  // call the following callback
+  var project = models.Project.find({"_id": projectID}).exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -16,6 +15,19 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  var newProject = new models.Project(form_data);
+  newProject['title'] = form_data['project_title'];
+  newProject['image'] = form_data['image_url'];
+
+  console.log(newProject);
+
+  newProject.save(afterSaving);
+
+  function afterSaving(err) {
+    if (err) {console.log(err); res.send(500);}
+    res.send(200);
+  }
+
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
 }
@@ -23,6 +35,10 @@ exports.addProject = function(req, res) {
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
-  // find the project and remove it
-  // YOU MUST send an OK response w/ res.send();
+  var project = models.Project.find({"_id": projectID}).remove().exec(afterRemoval);
+
+  function afterRemoval(err) {
+    if (err) {console.log(err); res.send(500);}
+    res.send(200);
+  }
 }
